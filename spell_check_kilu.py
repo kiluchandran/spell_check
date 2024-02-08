2c5c69f6f7da6cda96b0f3406f8767ed003db146
kiluchandran <kiluchandran@gmail.com>
2024-02-08 16:28:34 +0530

    Implemented positional argument using argparse module

---
+++ spell_check_kilu.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file")
+args = parser.parse_args()
+
+
+def main():
+    file_name = args.input_file
+    file1 = open(file_name, "r")
+    lines = file1.readlines()
+
+
+main()
